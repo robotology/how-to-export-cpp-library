4e28ae0ae0f105437493b0c02217243062e54ef1
Francesco Romano <francesco.romano.1987@gmail.com>
2014-10-06 17:29:21 +0200

    Added support to RPATH

---
+++ src/ExeExample/main.cpp
@@ -0,0 +1,34 @@
+#include  "template-lib.h"
+
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Example executable" << std::endl;
+   LibTemplateCMake::summationClass sumClass;
+   LibTemplateCMake::differenceClass diffClass;
+   
+   double tol = 1e-10;
+   double op1 = 15.0;
+   double op2 = 10.0;
+   
+   if( fabs(sumClass.doSomething(op1,op2)-(op1+op2)) > tol ) 
+   {
+       std::cerr << "[ERR] sumClass.doSomething(" << op1 << "," << op2 
+                 << ") is equal to " << sumClass.doSomething(op1,op2) 
+                 << " instead of the expected " << op1+op2 << std::endl; 
+       return EXIT_FAILURE;
+   }
+   
+   if( fabs(diffClass.doSomething(op1,op2)-(op1-op2)) > tol ) 
+   {
+       std::cerr << "[ERR] sumClass.doSomething(" << op1 << "," << op2 
+                 << ") is equal to " << diffClass.doSomething(op1,op2) 
+                 << " instead of the expected " << op1-op2 << std::endl; 
+       return EXIT_FAILURE;
+   }
+   
+   return EXIT_SUCCESS;
+}

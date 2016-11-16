71c0d35dd3409d5e16c7d568498be302c99c0c6b
Silvio <pegua1@gmail.com>
2016-11-16 14:51:51 +0100

    Add missing files

---
+++ src/LibTemplateCMake/include/LibTemplateCMake/LibTemplateCMake.h
@@ -0,0 +1,64 @@
+#ifndef LIB_TEMPLATE_CMAKE_H
+#define LIB_TEMPLATE_CMAKE_H
+
+namespace LibTemplateCMake {
+
+/**
+ * \class LibTemplateCMake::aClass
+ * \headerfile template-lib.h <TemplateLib/templatelib.h>
+ *
+ * \brief A class from LibTemplateCMake namespace.
+ *
+ * This class that does a summation.
+ */
+class summationClass
+{
+public:
+    /**
+     * Constructor
+     */
+    summationClass();
+
+    /**
+     * Destructory
+     */
+    virtual ~summationClass();
+
+    /**
+     * A method that does a summation
+     */
+    virtual double doSomething(double op1, double op2);
+};
+
+
+/**
+ * \class LibTemplateCMake::anotherClass
+ * \headerfile template-lib.h <TemplateLib/templatelib.h>
+ *
+ * \brief A derived class from LibTemplateCMake namespace.
+ *
+ * This class performs a difference.
+ */
+class differenceClass : public summationClass
+{
+public:
+    /**
+     * Constructor
+     */
+    differenceClass();
+
+    /**
+     * Destructory
+     */
+    virtual ~differenceClass();
+
+    /**
+     * A method that does something
+     */
+    virtual double doSomething(double op1, double op2);
+};
+
+
+} // namespace LibTemplateCMake
+
+#endif // LIB_TEMPLATE_CMAKE_H
---
+++ src/LibTemplateCMake/src/LibTemplateCMake.cpp
@@ -0,0 +1,28 @@
+#include "LibTemplateCMake/LibTemplateCMake.h"
+
+LibTemplateCMake::summationClass::summationClass()
+{
+}
+
+LibTemplateCMake::summationClass::~summationClass()
+{
+}
+
+double LibTemplateCMake::summationClass::doSomething(double op1, double op2)
+{
+    return op1+op2;
+}
+
+LibTemplateCMake::differenceClass::differenceClass() :
+        summationClass()
+{
+}
+
+LibTemplateCMake::differenceClass::~differenceClass()
+{
+}
+
+double LibTemplateCMake::differenceClass::doSomething(double op1, double op2)
+{
+    return op1-op2;
+}
